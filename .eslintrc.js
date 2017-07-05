98edf706195840678b3914e6f6d3099ffbcbef78
Tomasz Modrzyński <dev@modriv.net>
2017-07-06 00:38:57 +0200

    Add Meteor-specific rules to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,21 @@
 module.exports = {
-  extends: 'airbnb-base',
+  plugins: ['meteor'],
+  extends: ['airbnb-base', 'plugin:meteor/recommended'],
   env: {
     browser: true,
     meteor: true,
     es6: true,
+  },
+  rules: {
+    'import/prefer-default-export': 'off',
+
+    // ESLint doesn't like Meteor imports not being in package.json - let's turn it off
+    // See: https://github.com/clayne11/eslint-import-resolver-meteor/issues/11#issuecomment-238267628
+    'import/no-extraneous-dependencies': 'off',
+    // It also cries about imports like meteor/meteor not having an extension
+    'import/extensions': ['off', 'never']
+  },
+  settings: {
+    'import/resolver': ['meteor'],
   },
 };
